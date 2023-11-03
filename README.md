2696ee1d27b78f4645438af5114c11c134bfd1e1
Roger Farrerons <rugifa@hotmail.com>
2023-11-03 19:05:59 +0100

    add readme

--- README.md
+++ README.md
@@ -11,3 +11,26 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 # npx hardhat run scripts/deploy.js
 ```
+
+### Testing list
+
+All the contracts of the project have been tested. Below is a list of the tests carried out for each contract.
+
+[FactoryLP.sol](test/FactoryLP.js): This contract is a factory of liquidity pools (**8 tests**)
+
+[FactoryP2P.sol](test/FactoryP2P.js): This contract is a factory of P2P smart contract (**5 tests**)
+
+[FLTokenV2.sol](test/FLTokenV2.js): This contract is the implementation of an ERC20 token (**8 tests**)
+
+[LiquidityPool.sol](test/LiquidityPoolV2.js): This contract is a liquidity pool for an ERC20 token in a development environment (**8 tests**)
+
+[LPImplementation.sol](test/LiquidityPoolV2.js): Implementation of a liquidity pool to be deployed by the factory (**8 tests**)
+
+[NodeManager.sol](test/NodeManager.js): This contract allows for the deposit and withdrawal of ERC20 tokens. (**12 tests**)
+
+[P2PDEXImplementation.sol](test/P2PDEXImplementation.js): This contract allows the generation of offers to buy and sell ERC20 tokens (**21 tests**)
+
+[WLFactory.sol](test/WLFactory.js): This contract is a whitelist contract factory (**11 tests**)
+
+[WLImplementation.sol](test/WLImplementation.js): This contract allows adding and removing users from a whitelist (**17 tests**)
+
--- test/FactoryP2P.js
+++ test/FactoryP2P.js
@@ -4,8 +4,9 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { ethers } = require("hardhat");
 const { eth } = require("web3");
+const P2PABI = require("./../ABIs/P2PABI.json");
 
-describe("P2PDEX implementation", function () {
+describe("Factor P2PDEX", function () {
   async function deployContractsFixture() {
     const [owner, otherAccount, otherAccount2] = await ethers.getSigners();
 
@@ -18,17 +19,118 @@ describe("P2PDEX implementation", function () {
       18
     );
 
+    const decimals = await token.decimals();
+
     const LiquidityPool = await ethers.getContractFactory("LPImplementation");
     const liquidityPool = await LiquidityPool.deploy(token.target, owner);
+
+    const eth_amount = "10";
+    const token_amount = BigInt(10000) * BigInt(10) ** decimals;
+    const eth_amount_wei = ethers.parseEther(eth_amount);
+
+    await token.connect(owner).approve(liquidityPool.target, token_amount);
+    await liquidityPool.connect(owner).initializePool(token_amount, {
+      value: eth_amount_wei,
+    });
+
+    const P2PFactory = await ethers.getContractFactory(
+      "TokenExchangeP2PFactory"
+    );
+
+    const p2pfactory = await P2PFactory.deploy();
 
     return {
       token,
       liquidityPool,
       owner,
       otherAccount,
-      decimals,
-      p2pdex,
+      p2pfactory,
       otherAccount2,
+      decimals,
     };
   }
+
+  describe("Create P2P", function () {
+    it("Should deploy a P2P contract", async function () {
+      const { owner, p2pfactory, otherAccount, liquidityPool } =
+        await loadFixture(deployContractsFixture);
+
+      await p2pfactory.createTokenExchangeP2P(liquidityPool.target);
+      arrayP2P = await p2pfactory.getDeployedExchanges();
+
+      expect(arrayP2P.length).to.equal(1);
+    });
+
+    it("Should interact with P2P contract created", async function () {
+      const { owner, p2pfactory, otherAccount, liquidityPool, token } =
+        await loadFixture(deployContractsFixture);
+
+      await p2pfactory.createTokenExchangeP2P(liquidityPool.target);
+      arrayP2P = await p2pfactory.getDeployedExchanges();
+
+      const P2PContract = new ethers.Contract(
+        arrayP2P[0],
+        P2PABI,
+        ethers.provider
+      );
+      tokenAddress = await P2PContract.getTokenAddress();
+      expect(tokenAddress).to.equal(token.target);
+    });
+
+    it("Should create an offer with P2P contract created", async function () {
+      const { owner, p2pfactory, liquidityPool } = await loadFixture(
+        deployContractsFixture
+      );
+
+      eth_amount = "0.8";
+      eth_amount_wei = ethers.parseEther(eth_amount);
+
+      await p2pfactory.createTokenExchangeP2P(liquidityPool.target);
+      arrayP2P = await p2pfactory.getDeployedExchanges();
+
+      const P2PContract = new ethers.Contract(
+        arrayP2P[0],
+        P2PABI,
+        ethers.provider
+      );
+
+      await P2PContract.connect(owner).createOfferBuyTokens({
+        value: eth_amount_wei,
+      });
+      [seller, tokenAmount, etherAmount, status] =
+        await P2PContract.getOfferBuyTokensById(0);
+
+      expect(status).to.equal(1);
+      expect(seller).to.equal(owner.address);
+    });
+
+    it("Should deploy a P2P contract by other user (not owner)", async function () {
+      const { p2pfactory, otherAccount, liquidityPool } = await loadFixture(
+        deployContractsFixture
+      );
+
+      await p2pfactory
+        .connect(otherAccount)
+        .createTokenExchangeP2P(liquidityPool.target);
+      arrayP2P = await p2pfactory.getDeployedExchanges();
+
+      expect(arrayP2P.length).to.equal(1);
+    });
+
+    it("Shouldn't deploy a P2P if is existing", async function () {
+        const { p2pfactory, otherAccount, liquidityPool } = await loadFixture(
+          deployContractsFixture
+        );
+  
+        await p2pfactory
+          .connect(otherAccount)
+          .createTokenExchangeP2P(liquidityPool.target);
+        arrayP2P = await p2pfactory.getDeployedExchanges();
+  
+        expect(arrayP2P.length).to.equal(1);
+
+        await expect(p2pfactory.createTokenExchangeP2P(liquidityPool.target)).to.be.revertedWith("P2P already created")
+      });
+    
+  });
 });
